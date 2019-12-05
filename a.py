8fb50a3c6655591d00a317d37fe9206b66daf3c7
Honokadaisuki <orange_cheers@outlook.com>
2019-12-05 10:12:52 +0800

    Query

--- a.py
+++ a.py
@@ -21,7 +21,6 @@ def setImage(imagepath):
     img.convert("RGB").save(output, "BMP") #以RGB模式保存图像
     data = output.getvalue()[14:]
     output.close()
-    setImage(data)
     clip.OpenClipboard() #打开剪贴板
     clip.EmptyClipboard()  #先清空剪贴板
     clip.SetClipboardData(win32con.CF_DIB, data)  #将图片放入剪贴板
@@ -31,7 +30,31 @@ def readimg(path):
     with open(path,'rb') as fp:
         return fp.read()
 
+def holog(info):
+    with open("index.ho","a+",encoding='gbk') as f:
+        f.write(info)
 
-if __name__ == '__main__':
-    namelist = os.listdir("pic");
-    print(namelist)
+def init():
+    listname = os.listdir("pic")
+    for tn in listname:
+        ta = tn+"@*@";
+        js = client.basicGeneral(readimg("pic\\"+tn))
+        for wo in js['words_result']:
+            ta = ta + wo['words'] + '@*@'
+        ta = ta + '\r\n'
+        print(ta)
+        holog(ta)
+
+cont = []
+menu = []
+with open("index.ho",encoding='gbk') as f:
+    cont = f.readlines()
+finds = input()
+tcnt = 0
+for tstr in cont:
+    if(tstr.find(finds) != -1):
+        print(str(tcnt) + " : " + tstr)
+        tcnt = tcnt + 1
+        menu.append(tstr.split("@*@")[0])
+chosen = input()
+setImage("pic/"+menu[int(chosen)])
