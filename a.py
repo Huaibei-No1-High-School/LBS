2d096c80a4a4dbe92576c929ea57d727ed8987a4
Honokadaisuki <orange_cheers@outlook.com>
2019-12-05 08:28:40 +0800

    ?
    gs

---
+++ a.py
@@ -0,0 +1,24 @@
+
+import win32clipboard as clip
+import win32con
+from PIL import Image
+from io import StringIO
+from io import BytesIO
+
+'''
+    往剪贴板中放入图片
+'''
+def setImage(data):
+    clip.OpenClipboard() #打开剪贴板
+    clip.EmptyClipboard()  #先清空剪贴板
+    clip.SetClipboardData(win32con.CF_DIB, data)  #将图片放入剪贴板
+    clip.CloseClipboard()
+
+if __name__ == '__main__':
+    imagepath = 'top1.png'
+    img = Image.open(imagepath)# Image.open可以打开网络图片与本地图片。
+    output = BytesIO()  # BytesIO实现了在内存中读写bytes
+    img.convert("RGB").save(output, "BMP") #以RGB模式保存图像
+    data = output.getvalue()[14:]
+    output.close()
+    setImage(data)
