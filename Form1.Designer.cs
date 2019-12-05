3f4e86c65a6ebe6bf88d2723be040fabc0eafc01
Honokadaisuki <orange_cheers@outlook.com>
2019-12-05 13:09:53 +0800

    V1.0

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(633, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 64);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "/ 450";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(529, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 64);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "449";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(515, 97);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 62);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "更新";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(491, 53);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(12, 71);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(491, 364);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(509, 194);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(279, 241);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.DoubleClick += new System.EventHandler(this.pictureBox1_DoubleClick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(659, 76);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(129, 112);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +15,181 @@ namespace LBS
 {
     public partial class Form1 : Form
     {
+        string path = @"pic";
+        string indexpath = @"index.ho";
+        string cntpath = @"cnt.ho";
+        string APP_ID = "17942189";
+        string API_KEY = "dD4bDfmKj9QGvGz84XvVLwaV";
+        string SECRET_KEY = "7siy7rVkth8UKsB3dO3syZ8zXj1fmgRc";
+        List<string> al;
+        List<string> cont;
+        Baidu.Aip.Ocr.Ocr client;
         public Form1()
         {
             InitializeComponent();
+            client = new Baidu.Aip.Ocr.Ocr(API_KEY, SECRET_KEY);
+            al = new List<string>();
+            cont = new List<string>();
+            updatelist();
+            updatelv();
+        }
+        private void updatelist()
+        {
+            cont.Clear();
+            al.Clear();
+            string []tss = File.ReadAllLines(indexpath, Encoding.GetEncoding("GBK"));
+            foreach(string s in tss)
+            {
+                if(s.Trim() != "")
+                {
+                    cont.Add(s.Trim());
+                }
+            }
+            Console.WriteLine(cont.Count);
+            foreach(string s in cont)
+            {
+                al.Add(Regex.Split(s, "@*@")[0]);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            updatelist();
+            Checknewfile();
+            textBox1.Select();
+            textBox1.Focus();
+            pictureBox2.Image = Resource1.no;
+        }
+        private bool Checknewfile()
+        {
+            var files = Directory.GetFiles(path, "*.*");
+            string has = File.ReadAllText(cntpath).Trim();
+            has = (Convert.ToInt32(has) + 1).ToString();
+            string havnt = files.Count().ToString();
+            label1.Text = "/ " + havnt;
+            label2.Text = has;
+            if(has != havnt)
+            {
+                label2.ForeColor = Color.Red;
+                return true;
+            }
+            else
+            {
+                label2.ForeColor = Color.Black;
+                return false;
+            }
+        }
+        int getcnt()
+        {
+            string has = File.ReadAllText(cntpath).Trim();
+            return Convert.ToInt32(has);
+        }
+        private void addimg(string tpath)
+        {
+            //读取编号
+            int tcnt = getcnt()+1;
+            string filename = "";
+            //OCR 添加index
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(indexpath,true,Encoding.GetEncoding("GBK")))
+            {
+                var res = client.General(File.ReadAllBytes(tpath));
+                string st = tcnt.ToString() + "." + tpath.Split('.')[1];
+                filename = st;
+                st += "@*@";
+                foreach (var str in res["words_result"])
+                {
+                    st += str["words"].ToString() + "@*@";
+                }
+                Console.WriteLine(st);
+                file.WriteLine(st);
+            }
+            //添加图片
+            FileStream fs = new FileStream(path + "/" + filename, FileMode.Create);
+            BinaryWriter bw = new BinaryWriter(fs);
+            bw.Write(File.ReadAllBytes(tpath), 0, File.ReadAllBytes(tpath).Length);
+            bw.Close();
+            fs.Close();
+            //写入编号
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(cntpath,false, Encoding.GetEncoding("GBK")))
+            {
+                file.WriteLine(tcnt);
+            }
+            //更新List
+            updatelist();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //frcnt();
+            
+        }
+
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.All;                                                              //重要代码：表明是所有类型的数据，比如文件路径
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+
+            Console.WriteLine("!");
+            string tpath = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            addimg(tpath);
+        }
+        private void updatenewfiles()
+        {
+            var files = Directory.GetFiles(path, "*.*");
+            foreach (string s in files)
+            {
+                string ts = Path.GetFileName(s);
+                if (!al.Contains(ts))
+                {
+                    addimg(path + "/" + ts);
+                    File.Delete(path + "/" + ts);
+                }
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            updatelist();
+            if (Checknewfile())
+            {
+                updatenewfiles();
+            }
+            Checknewfile();
+            updatelv();
+        }
+        private void updatelv()
+        {
+            listBox1.Items.Clear();
+            foreach (string s in cont)
+            {
+                if(s.Contains(textBox1.Text))
+                {
+                    listBox1.Items.Add(s);
+                }
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            updatelv();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(listBox1.SelectedItems.Count > 0)
+            {
+                pictureBox1.Load(path + "/" + Regex.Split(listBox1.SelectedItem.ToString(), "@*@")[0]);
+                pictureBox2.Image = Resource1.no;
+            }
+        }
+
+        private void pictureBox1_DoubleClick(object sender, EventArgs e)
+        {
+            Clipboard.SetImage(pictureBox1.Image);
+            pictureBox2.Image = Resource1.ok;
         }
     }
 }
